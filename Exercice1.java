52af7a4e9c03784fc47ac4730e3fcbcce5ca22f0
LittleSoap <32540199+LittleSoap@users.noreply.github.com>
2018-12-05 22:01:12 +0100

    Add files via upload

---
+++ Exercice1.java
@@ -0,0 +1,31 @@
+package labs.one.parta;
+
+public class Exercice1{
+
+    public static void main(String[] args){
+
+        for(int i=0;i<args.length;i++){
+            System.out.println(i+1+") "+args[i]);
+        }
+
+        
+
+    }
+
+
+}
+
+
+/*
+
+Exercice 1
+
+1) False 20
+2) Result : 9
+3) Ok tout le temps (affectation au lieu de comparaison)
+4) Erreur variable non initialisée
+5) 12
+
+
+
+*/
