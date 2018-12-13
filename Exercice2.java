377b6eb8b81da4246e9e4565bae1dcd0449c9059
LittleSoap <32540199+LittleSoap@users.noreply.github.com>
2018-12-13 12:43:18 +0100

    Exo 3

--- Exercice2.java
+++ Exercice2.java
@@ -1,89 +1,100 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+package exercises;
+
+import java.io.*;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Exercice2{
+public class Exercice2 {
 
-    public Exercice2(){}
+    public Exercice2() {
+    }
 
     BufferedReader reader;
 
-    public void readFile(String text) throws IOException {
+    public void readFile(String text){
 
-        reader = new BufferedReader(new FileReader(text));
-        List<Integer> arrayList = new ArrayList<Integer>();
+        try {
 
-        String line;
+            reader = new BufferedReader(new FileReader(text));
+            List<Integer> arrayList = new ArrayList<Integer>();
 
-        line = reader.readLine();
+            String line;
 
-        while(line != null){
-
-            int score = Integer.parseInt(line.split(":")[1]);
-
-            System.out.println(line);
-            System.out.println(score);
-            arrayList.add(score);
             line = reader.readLine();
-        }
 
-        int moyenne = 0;
+            while (line != null) {
 
-        for(int i = 0; i<arrayList.size(); i++){
-            moyenne += arrayList.get(i);
-        }
+                int score = Integer.parseInt(line.split(":")[1]);
 
-        moyenne /= arrayList.size();
-
-        System.out.println("Moyenne : " + moyenne);
-
-
-
-    }
-
-    public void getExecutionTime(String text, String email)throws IOException{
-
-        Instant start = Instant.now();
-
-
-        this.getStudentGrade(text, email);
-
-
-        Duration duration = Duration.between(start,Instant.now());
-        System.out.println("Temps d'exécution du programme : " + duration.toString().substring(2).replaceAll("(\\d[HMS])(?!$)", "$1 ").toLowerCase());
-
-
-    }
-
-
-    public void getStudentGrade(String text, String email) throws IOException{
-
-        reader = new BufferedReader(new FileReader(text));
-        String line = reader.readLine();
-        String score = "";
-
-        while(line != null){
-
-            if(line.split(":")[0].equals(email)){
-                score = line.split(":")[1];
+                System.out.println(line);
+                System.out.println(score);
+                arrayList.add(score);
+                line = reader.readLine();
             }
 
-            line = reader.readLine();
+            int average = 0;
 
+            for (int i = 0; i < arrayList.size(); i++) {
+                average += arrayList.get(i);
+            }
+
+            average /= arrayList.size();
+
+            System.out.println("Moyenne : " + average);
+
+        }catch (FileNotFoundException e){
+            //e.printStackTrace();
+            System.out.println("Fichier non trouvé");
+        }catch (Exception e){
+            System.out.println("Fichier inaccessible");
         }
+    }
 
-        if(score.equals("")){
-            System.out.println("Pas de note trouvée pour " + email);
-        }else{
-            System.out.println("Note pour " + email + " : " + score);
+    public void getExecutionTime(String text, String email)
+    {
+        try {
+
+            Instant start = Instant.now();
+            this.getStudentGrade(text, email);
+
+            Duration duration = Duration.between(start, Instant.now());
+            System.out.println("Temps d'exécution du programme : " + duration.toString().substring(2).replaceAll("(\\d[HMS])(?!$)", "$1 ").toLowerCase());
+
+        }catch(Exception e){
+            e.printStackTrace();
         }
 
     }
 
+    public String getStudentGrade(String text, String email){
+
+        try {
+            reader = new BufferedReader(new FileReader(text));
+            String line = reader.readLine();
+            String score = "";
+
+            while (line != null) {
+
+                if (line.split(":")[0].equals(email)) {
+                    score = line.split(":")[1];
+                }
+
+                line = reader.readLine();
+            }
+
+            if (!score.equals("")) {
+                return score;
+            }
+
+            return "";
+
+        }catch (Exception e){
+            e.printStackTrace();
+            return "";
+        }
+    }
 
 
 }
